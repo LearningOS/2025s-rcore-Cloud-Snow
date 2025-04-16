4b80e6cc09f4e3898a2095ceb68f9b7525fe9e79
Cloud <313670299@qq.com>
2025-04-16 20:58:56 +0800

    完成chapter3

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,6 +1,6 @@
 //! Process management syscalls
 use crate::{
-    task::{exit_current_and_run_next, suspend_current_and_run_next},
+    task::{exit_current_and_run_next, get_syscall_count, suspend_current_and_run_next},
     timer::get_time_us,
 };
 
@@ -41,5 +41,23 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 => {
+            let ptr = _id as *const u8;
+            return unsafe { *ptr } as isize;
+        }
+        1 => {
+            let ptr = _id as *mut u8;
+            unsafe {
+                *ptr = (_data & 0xFF) as u8;
+            }
+            return 0;
+        }
+        2 => {
+            return get_syscall_count(_id) as isize;
+        }
+        _ => {
+            return -1;
+        }
+    }
 }
